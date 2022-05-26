90ad447da17ae4f302c13a6b4982f6de093ba835
Gustavo Ferreira <82911312+gustaferreira@users.noreply.github.com>
2022-05-25 23:24:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>2023</title>
+        <link rel="stylesheet" href="style.css"/>
+        <script src="script.js" defer></script>
+    </head>
+    <body>
+        <header>
+            <h1>Feliz 2023!!</h1>
+
+            <div class="container">
+                <div class="container-dentro">
+                    <p class="texto" id="dias">0</p>
+                    <span>dias</span>
+                </div>
+                <div class="container-dentro">
+                    <p class="texto" id="horas">0</p>
+                    <span>horas</span>
+                </div>
+                <div class="container-dentro">
+                    <p class="texto" id="minutos">0</p>
+                    <span>minutos</span>
+                </div>
+                <div class="container-dentro">
+                    <p class="texto" id="segundos">0</p>
+                    <span>segundos</span>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="texto-zoera">
+                <button>
+                    #Você é gay hehehe!
+                </button>
+            </div>
+        </main>
+    </body>
+</html>
